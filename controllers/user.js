3b2da482325981985f6cfbec4e36cc8c34bbbf86
Martin-J-Larre <martinjlarre@gmail.com>
2023-02-22 00:49:03 -0500

    #10 update user router created, multer for upload file with route and method

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const fs = require("fs");
 
 const validate = require("../helpers/validate");
 const UserModel = require("../models/User");
@@ -136,25 +137,100 @@ exports.getUserProfile = (req, res) => {
 };
 
 exports.updateUser = (req, res) => {
-  // const userAuthData = req.user;
-  // const dataToUpdate = req.body;
+  const userAuthData = req.user;
+  const dataToUpdate = req.body;
 
-  // UserModel.find({
-  //   $0r: [
-  //     { email: dataToUpdate.email.toLowerCase() },
-  //     { nickname: dataToUpdate.nickname.toLowerCase() },
-  //   ],
-  // }).exec((error, users) => {
-  //   if (error) {
-  //     return res.status(500).json({
-  //       status: "error",
-  //       message: "Error user could not be found",
-  //     });
-  //   }
-  // });
+  //TODO: Figurate out validate() for update
 
-  return res.status(200).json({
-    status: "success",
-    message: "User updated successfully",
+  UserModel.find({
+    $or: [{ email: dataToUpdate.email }, { nickname: dataToUpdate.nickname }],
+  }).exec(async (error) => {
+    if (error) {
+      return res.status(500).json({
+        status: "error",
+        message: "Error user could not be found",
+      });
+    }
+
+    if (dataToUpdate.password) {
+      let passwordHashed = await bcrypt.hash(dataToUpdate.password, 10);
+      dataToUpdate.password = passwordHashed;
+    } else {
+      delete dataToUpdate.password;
+    }
+
+    try {
+      const userUpdated = await UserModel.findByIdAndUpdate(
+        { _id: userAuthData.id },
+        dataToUpdate,
+        { new: true }
+      );
+
+      if (!userUpdated) {
+        return res.status(400).json({
+          status: "error",
+          message: "Error user could not be updated",
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+        message: "User updated successfully",
+        userUpdated,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: "Error user could not be updated",
+      });
+    }
   });
+};
+
+exports.uploadAvatar = (req, res) => {
+  if (!req.file) {
+    return res.status(404).json({
+      status: "error",
+      message: "Error file do not exist",
+      file: req.file,
+    });
+  }
+
+  const nameFile = req.file.originalname;
+  const nameFileSplit = nameFile.split(".");
+  const extension = nameFileSplit[1];
+
+  if (
+    extension != "png" &&
+    extension != "jpg" &&
+    extension != "jpeg" &&
+    extension != "gif"
+  ) {
+    const filePath = req.file.path;
+    const deleteFile = fs.unlinkSync(filePath);
+
+    return res.status(404).json({
+      status: "error",
+      message: "Error extencion file is not valid",
+    });
+  }
+
+  UserModel.findOneAndUpdate(
+    { _id: req.user.id },
+    { image: req.file.filename },
+    { new: true },
+    (error, userUpdated) => {
+      if (error || !userUpdated) {
+        return res.status(404).json({
+          status: "error",
+          message: "Error file could not be updated",
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+        message: "File updated successfully",
+        file: req.file,
+        userUpdated,
+      });
+    }
+  );
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const userController = require("../controllers/user");
 const { isAuth } = require("../middlewares/auth");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/avatars/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "avatar-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage });
+
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.post(
+  "/upload",
+  [isAuth, uploads.single("file")],
+  userController.uploadAvatar
+);
 router.get("/profile/:id", isAuth, userController.getUserProfile);
 router.put("/update", isAuth, userController.updateUser);
 
