3cb0fc6de5c1f297f040fa694555471c7ab879e9
Martin-J-Larre <martinjlarre@gmail.com>
2023-02-26 12:32:49 -0500

    #16 add route and controller for update data song
    route and controller for delete song
    
    route and controller for update file audio
    
    route and controller for get file audio

--- controllers/song.js
+++ controllers/song.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const SongModel = require("../models/Song");
 
 exports.createSong = (req, res) => {
@@ -65,9 +68,106 @@ exports.getAllSongs = (req, res) => {
 };
 
 exports.updateSong = (req, res) => {
-  const sondId = req.params.id;
-  return res.status(200).json({
-    status: "success",
-    message: "Song updated successfully",
+  const songId = req.params.id;
+  const dataToUpdate = req.body;
+
+  SongModel.findByIdAndUpdate(
+    songId,
+    dataToUpdate,
+    { new: true },
+    (error, songUpdated) => {
+      if (error || !songUpdated) {
+        return res.status(500).json({
+          status: "error",
+          message: "Error song could not be updated ",
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        message: "Song updated successfully",
+        songUpdated,
+      });
+    }
+  );
+};
+
+exports.deleteSong = (req, res) => {
+  const songId = req.params.id;
+
+  SongModel.findByIdAndRemove(songId, (error, songDeleted) => {
+    if (error || !songDeleted) {
+      return res.status(404).json({
+        status: "error",
+        message: "Error song could not be deleted",
+      });
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: "Song deleted successfully",
+      songDeleted,
+    });
+  });
+};
+
+exports.uploadFile = (req, res) => {
+  if (!req.file) {
+    return res.status(404).json({
+      status: "error",
+      message: "Error file do not exist",
+    });
+  }
+
+  const nameFile = req.file.originalname;
+  const nameFileSplit = nameFile.split(".");
+  const extension = nameFileSplit[1];
+
+  if (extension != "mp3" && extension != "ogg") {
+    const filePath = req.file.path;
+    const deleteFile = fs.unlinkSync(filePath);
+
+    return res.status(404).json({
+      status: "error",
+      message: "Error format file is not valid",
+      deleteFile,
+    });
+  }
+
+  AlbumModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { file: req.file.filename },
+    { new: true },
+    (error, songUpdated) => {
+      if (error || !songUpdated) {
+        return res.status(404).json({
+          status: "error",
+          message: "Error file could not be updated",
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+        message: "File updated successfully",
+        file: req.file,
+        songUpdated,
+      });
+    }
+  );
+};
+
+exports.getFile = (req, res) => {
+  const file = req.params.file;
+
+  const filePath = "./uploads/songs/" + file;
+
+  fs.stat(filePath, (error, exists) => {
+    if (error || !exists) {
+      return res.status(404).json({
+        status: "error",
+        message: "File does not exist",
+      });
+    }
+
+    return res.sendFile(path.resolve(filePath));
   });
 };
--- routes/song.js
+++ routes/song.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const songController = require("../controllers/song");
 const { isAuth } = require("../middlewares/auth");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/songs/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "song-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage });
 
 router.post("/create", isAuth, songController.createSong);
 router.get("/name/:id", isAuth, songController.getOneSong);
 router.get("/names/:albumId", isAuth, songController.getAllSongs);
 router.put("/update/:id", isAuth, songController.updateSong);
+router.delete("/delete/:id", isAuth, songController.deleteSong);
+router.post(
+  "upload/:id",
+  [isAuth, uploads.single("file")],
+  songController.uploadFile
+);
+router.get("/audio/:file", isAuth, songController.getFile);
 
 module.exports = router;
